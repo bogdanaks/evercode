f1ad8b86242d4ce0f08d6052173387375378bb4b
bogdanaks <bogdanaks@bk.ru>
2020-11-13 18:45:02 +0300

    Create object

---
+++ client/src/utils/icons.js
@@ -0,0 +1,21 @@
+import BTC from '../assets/icons/btc.svg'
+import ETH from '../assets/icons/eth.svg'
+import XRP from '../assets/icons/xrp.svg'
+import BCH from '../assets/icons/bch.svg'
+import BSV from '../assets/icons/bsv.svg'
+import LTC from '../assets/icons/ltc.svg'
+import DASH from '../assets/icons/dash.svg'
+import ZEC from '../assets/icons/zec.svg'
+import DOGE from '../assets/icons/doge.svg'
+
+export const icons = {
+    BTC,
+    ETH,
+    XRP,
+    BCH,
+    BSV,
+    LTC,
+    DASH,
+    ZEC,
+    DOGE,
+}
