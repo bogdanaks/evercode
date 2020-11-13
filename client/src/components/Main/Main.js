7e51b7e2b650f04f88a472526a4cdb785d3b1e72
bogdanaks <bogdanaks@bk.ru>
2020-11-13 18:44:51 +0300

    Fix and add handle for del

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,29 +1,46 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 import styles from './styles.module.scss'
+import { icons } from '../../utils/icons'
 
 import { Select } from '../Select/Select'
 import { Input } from '../Input/Input'
-import { useCryptoBalance } from '../../utils/hooks/useCryptoBalance/useCryptoBalance'
+import { useCryptoBalance } from '../../utils/hooks/useCryptoBalance'
+import { useExportCSV } from '../../utils/hooks/useExportCSV'
+import { useError } from '../../utils/hooks/useError/useError'
 
 export const Main = () => {
     const [list, setList] = React.useState([])
     const [input, setInput] = React.useState('')
     const [select, setSelect] = React.useState({ title: 'SELECT', desc: '' })
 
-    const { getBalance, loader } = useCryptoBalance(select.title, input)
+    const { Error, show } = useError()
+    const { getBalance } = useCryptoBalance(select.title, input, show)
+    const { Link } = useExportCSV(list, show)
 
     const handleAddClick = async () => {
-        const balance = await getBalance()
-        setList((prevState) => [...prevState, { select, address: input, cash: balance }])
-        setInput('')
+        if (input.length > 0) {
+            const balance = await getBalance()
+            if (balance !== undefined) {
+                setList((prevState) => [
+                    ...prevState,
+                    { info: select, address: input, cash: balance, uuid: uuidv4() },
+                ])
+                setInput('')
+            }
+        }
     }
+    const handleDelete = (uuid) => {
+        setList((prevState) => prevState.filter((i) => i.uuid !== uuid))
+    }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.mainBlock}>
                 <div className={styles.header}>
                     <h2>Get crypto balance</h2>
-                    <button>Export CSV</button>
+                    {Link}
                 </div>
                 <div className={styles.row}>
                     <Select select={select} setSelect={setSelect} />
@@ -32,21 +49,28 @@ export const Main = () => {
                 </div>
                 {list.length > 0 && (
                     <ul className={styles.list}>
-                        {list.map((el, index) => {
+                        {list.map((el) => {
                             return (
-                                <li key={index}>
+                                <li key={el.uuid}>
+                                    <img src={icons[el.info.title]} alt={el.info.title} />
                                     <div className={styles.title}>
-                                        <h4>{el.select.desc}</h4>
-                                        <span>{el.select.title}</span>
+                                        <h4>{el.info.desc}</h4>
+                                        <span>{el.info.title}</span>
                                     </div>
                                     <span className={styles.address}>{el.address}</span>
                                     <span className={styles.cash}>{el.cash}</span>
+                                    <div
+                                        className={styles.delete}
+                                        onClick={() => handleDelete(el.uuid)}>
+                                        X
+                                    </div>
                                 </li>
                             )
                         })}
                     </ul>
                 )}
             </div>
+            <Error />
         </div>
     )
 }
